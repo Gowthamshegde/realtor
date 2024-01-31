1d1ec9d248fe30ff107b28ff3182531d318d5eda
Gowthamshegde <sgowtham327@gmail.com>
2024-01-31 17:40:28 +0530

    new link added to chat bot

--- src/Pages/Bot.jsx
+++ src/Pages/Bot.jsx
@@ -4,7 +4,7 @@ export default function bot() {
   return (
     <div>
       <iframe
-        src="https://web.powerva.microsoft.com/environments/22307f64-4eb3-eb48-871e-a5f49e392c1e/bots/cr8e2_testBot/webchat?__version__=2"
+        src="https://web.powerva.microsoft.com/environments/22307f64-4eb3-eb48-871e-a5f49e392c1e/bots/cr8e2_realtorChatBot/webchat?__version__=2"
         frameborder="0"
         className="mx-auto w-[500px] h-[400px] rounded-lg shadow-xl"
         title="chatbot"
